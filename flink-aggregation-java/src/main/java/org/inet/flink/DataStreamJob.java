2c82508773d9c2d7ec299a4ddc7ab6d6a6a9623b
Kevin Nguyen <kk.nguyen@web.de>
2023-05-29 17:04:19 +0200

    Word count test function

---
+++ flink-aggregation-java/src/main/java/org/inet/flink/DataStreamJob.java
@@ -0,0 +1,94 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.inet.flink;
+
+// import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+
+
+
+/**
+ * Skeleton for a Flink DataStream Job.
+ *
+ * <p>For a tutorial how to write a Flink application, check the
+ * tutorials and examples on the <a href="https://flink.apache.org">Flink Website</a>.
+ *
+ * <p>To package your application into a JAR file for execution, run
+ * 'mvn clean package' on the command line.
+ *
+ * <p>If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+public class DataStreamJob {
+
+	public static void main(String[] args) throws Exception {
+		// Sets up the execution environment, which is the main entry point
+		// to building Flink applications.
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+		/*
+		 * Here, you can start creating your execution plan for Flink.
+		 *
+		 * Start with getting some data from the environment, like
+		 * 	env.fromSequence(1, 10);
+		 *
+		 * then, transform the resulting DataStream<Long> using operations
+		 * like
+		 * 	.filter()
+		 * 	.flatMap()
+		 * 	.window()
+		 * 	.process()
+		 *
+		 * and many more.
+		 * Have a look at the programming guide:
+		 *
+		 * https://nightlies.apache.org/flink/flink-docs-stable/
+		 *
+		 */
+
+		DataStream<Tuple2<String, Integer>> dataStream = env
+			.socketTextStream("localhost", 9999)
+			.flatMap(new Splitter())
+			.keyBy(value -> value.f0)
+			.window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
+			.sum(1);
+
+		dataStream.print();
+		
+		// Execute program, beginning computation.
+		env.execute("Flink Java API Skeleton");
+	}
+
+	public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+		@Override
+		public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) {
+			for (String word: sentence.split(" ")) {
+				out.collect(new Tuple2<String, Integer>(word, 1));
+			}
+		}
+	}
+}
