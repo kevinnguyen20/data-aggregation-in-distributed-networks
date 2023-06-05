f3e3348dc27ce5cd1ed9ea08acf4c721cf1bb352
Kevin Nguyen <kk.nguyen@web.de>
2023-06-05 16:25:34 +0200

    Measure the elapsed time

--- dataGeneration.sh
+++ dataGeneration.sh
@@ -2,10 +2,17 @@
 
 source config.sh
 
+start_time=$(date +%s%N)
 
-for ((i=1; i<=100; i++))
+for ((i=1; i<=25; i++))
 do
   JSON="{\"id\": $i, \"name\": \"Apple\", \"price\": 0.85}"
 
   echo $JSON | "$KAFKA_HOME/bin/kafka-console-producer.sh" --bootstrap-server $KAFKA_BOOTSTRAP_SERVERS --topic $CONSUMER_TOPIC
 done
+
+end_time=$(date +%s%N)
+
+elapsed_time=$((($end_time - $start_time) / 1000000))
+
+echo "Elapsed time: $elapsed_time ms"
